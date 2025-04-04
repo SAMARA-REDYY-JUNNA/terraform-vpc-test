460938db51f66a8805fe702564507dc1f60a8dbd
samarareddy <samara.junna@gmail.com>
2025-04-04 19:54:46 +0530

    local repo

---
+++ vpc.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+    #source = "../terraform-aws-vpc"
+    source = "git::https://github.com/Devops/terraform-aws-vpc.git?ref=main"
+    project_name = var.project_name
+    common_tags = var.common_tags
+    public_subnet_cidr = var.public_subnet_cidrs
+    private_subnet_cidr = var.private_subnet_cidrs
+    database_subnet_cidr = var.database_subent_cidrs
+    is_peering_required = var.is_peering_required
+}
